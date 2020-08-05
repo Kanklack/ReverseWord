0bd524ecd5e123860a1f8904417fc36fdabab3af
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 19:57:36 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim name As String = " "
+
+        Dim original, reverse As String
+
+        Dim EndOfWord As Integer
+
+        Console.Write("Give me a word to reverse: ")
+
+        original = Console.ReadLine
+
+        EndOfWord = original.Length() - 1
+
+        reverse = ""
+
+        For Counter As Integer = EndOfWord To 0 Step -1
+
+            reverse = reverse & original(Counter)
+
+        Next
+
+        Console.WriteLine("Your reverse word is: " & reverse)
+
+        Console.Read()
+
+    End Sub
+
+End Module
